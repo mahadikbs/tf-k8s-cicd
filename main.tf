17e87513bb6b7ac5ebfb53799e43186f023abb6c
Brahmadev Shivaji Mahadik <brahmadev.mahadik@yash.com>
2025-05-25 18:12:12 +0530

    updated changes

--- main.tf
+++ main.tf
@@ -26,15 +26,15 @@ resource "aws_instance" "terraform-test" {
 
     provisioner "remote-exec" {
         inline = [
+            "sudo su ec2-user",
             "sudo yum update -y",
             "sudo amazon-linux-extras install docker",
             "sudo yum install -y docker",
             "sudo service docker start",
             "sudo usermod -aG docker $(whoami)",
             "newgrp docker",
-            "docker ps",
             "docker login -u '${var.DOCKER_USERNAME}' --password '${var.DOCKER_PASSWORD}'",     
-            "docker pull mahadikbs/k8s-prom-grafana:latest",
+            "docker pull mahadikbs/k8s-prom-grafana",
             "docker run -d -p 8080:8080 -p 9090:9090 -p 16443:16443 -p 3000:3000 --name k8s-server mahadikbs/k8s-prom-grafana:latest",
         ]
       
