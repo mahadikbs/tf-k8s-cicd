4acc224e14c033eaa8423d5faa67ee96033a0878
Brahmadev Shivaji Mahadik <brahmadev.mahadik@yash.com>
2025-05-25 17:31:13 +0530

    updated changes

--- main.tf
+++ main.tf
@@ -31,6 +31,7 @@ resource "aws_instance" "terraform-test" {
             "sudo yum install -y docker",
             "sudo service docker start",
             "sudo usermod -a -G docker ec2-user",
+            "docker ps",
             "docker login -u '${var.DOCKER_USERNAME}' --password '${var.DOCKER_PASSWORD}'",     
             "docker pull mahadikbs/k8s-prom-grafana:latest",
             "docker run -d -p 8080:8080 -p 9090:9090 -p 16443:16443 -p 3000:3000 --name k8s-server mahadikbs/k8s-prom-grafana:latest",
