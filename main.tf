33ef585e371a0e54db859d57411a2081f2631ba7
Brahmadev Shivaji Mahadik <brahmadev.mahadik@yash.com>
2025-05-26 20:48:11 +0530

    docker-compose

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ resource "aws_instance" "terraform-test" {
             "sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose",
             "sudo chmod +x /usr/local/bin/docker-compose",
             "cd /home/ec2-user",
-            "newgrp docker",
+            "sudo newgrp docker",
             "docker-compose up -d"
          ]
       
