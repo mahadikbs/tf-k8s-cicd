3704e13f1815839a14e722a37f504971f9789266
Brahmadev Shivaji Mahadik <brahmadev.mahadik@yash.com>
2025-05-27 10:48:46 +0530

    updated with user-data

--- main.tf
+++ main.tf
@@ -55,15 +55,15 @@ resource "aws_instance" "terraform-test" {
     sudo yum update -y
     sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
-       
+    cd /home/ec2-user
+    docker-compose up -d   
      EOF    
 
     provisioner "remote-exec" {
         inline = [ 
             "sudo service docker start",
             "cd /home/ec2-user",
-            "sudo sh install-kubectl.sh",
-            "docker-compose up -d"
+            "sudo sh install-kubectl.sh"
          ]
       
     }
